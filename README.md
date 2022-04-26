8b59ed59ba629beee46855b6cc9b1cc55849ef9a
ankit <ankit.sanghavi87@gmail.com>
2022-04-27 00:49:04 +0530

    project fin

---
+++ README.md
@@ -0,0 +1,10 @@
+Make this script an executable
+```
+chmod 744 script.sh 
+```
+
+Then simply run it as :
+
+```
+./script.sh
+```
---
+++ setup.sh
@@ -0,0 +1,20 @@
+sudo apt-get upgrade
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update 
+yes Y | sudo apt-get install docker-ce docker-ce-cli containerd.io
+curl -SL https://github.com/docker/compose/releases/download/v2.4.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker login -u ankit1841 -p anA56sz3*D101
+sudo docker run -v
+sudo docker run --version
+sudo docker-compose --version
